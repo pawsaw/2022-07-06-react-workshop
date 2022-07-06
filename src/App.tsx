3ae2782668a14212f9911f97ec616e9a396df472
Pawel Sawicki <public@psawicki.de>
2022-07-06 14:59:17 +0200

    Adding BookList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import { useCallback, useEffect, useState } from 'react';
 import './App.css';
-import { Counter, OnValueChanged } from './components/Counter';
+import { BookList } from './components/BookList';
+import { books } from './data/books';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // useEffect(() => {
-  //   const int = setInterval(() => {
-  //     setCount((count) => count + 1);
-  //     console.log({ count });
-  //   }, 1000);
-
-  //   return () => clearInterval(int);
-  // }, [count]);
-
-  const onCounterValueChanged: OnValueChanged = useCallback(
-    (newValue: number) => setCount(newValue),
-    [],
-  );
-
   return (
     <div className="App">
-      Count: {count}
-      <Counter onValueChanged={onCounterValueChanged} value={count} />
+      <BookList books={books} />
     </div>
   );
 }
